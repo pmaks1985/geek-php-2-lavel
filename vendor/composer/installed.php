f14cb9cb222900aff8d2aa1f25f94cce1b0e20a8
pmaks <pmaks1985@gmail.com>
2021-02-26 15:45:46 +0300

    добавил Twig

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8535dbeb6a44b3b58ca1784fa6fb50aa9acfcdc4',
+    'name' => 'vendor_name/geek-php-2-lavel',
+  ),
+  'versions' => 
+  array (
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v1.35.0',
+      'version' => '1.35.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'daa657073e55b0a78cce8fdd22682fddecc6385f',
+    ),
+    'vendor_name/geek-php-2-lavel' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8535dbeb6a44b3b58ca1784fa6fb50aa9acfcdc4',
+    ),
+  ),
+);
