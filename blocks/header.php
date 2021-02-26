b44833d6a2bd2afa63a2664af25e3e1dc4abb3c3
pmaks <pmaks1985@gmail.com>
2021-02-26 16:59:35 +0300

    сделал 1 под а,б,в

--- blocks/header.php
+++
@@ -1,3 +0,0 @@
-<div class="row">
-    <h1>Lorem, ipsum dolor. Сегодня <?= date('d.m.Y'); ?></h1>
-</div>
--- index.php
+++ index.php
@@ -1,40 +1,32 @@
 <?php
+// подгружаем и активируем авто-загрузчик Twig-а
+require_once 'Twig/Autoloader.php';
+Twig_Autoloader::register();
 
-// 1. Создать галерею фотографий. Она должна состоять из одной страницы, на которой пользователь видит все картинки в уменьшенном виде. 
-// При клике на фотографию она должна открыться в браузере в новой вкладке. Размер картинок можно ограничивать с помощью свойства width.
+try {
+    // указывае где хранятся шаблоны
+    $loader = new Twig_Loader_Filesystem('template');
 
+    // инициализируем Twig
+    $twig = new Twig_Environment($loader);
+
+    // подгружаем шаблон
+    $template = $twig->loadTemplate('contents.tmpl');
+
+    // передаём в шаблон переменные и значения
+    // выводим сформированное содержание
+
+    $images = array_diff( scandir( "images"), array('..', '.'));
+    $date = date('d.m.Y');
+
+    $content = $template->render(array(
+        'images' => $images,
+        'date' => $date
+    ));
+
+    echo $content;
+
+} catch (Exception $e) {
+    die ('ERROR: ' . $e->getMessage());
+}
 ?>
-
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script type="text/javascript" src="scripts/jquery.js"></script>
-    <script type="text/javascript" src="scripts/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-    <script type="text/javascript" src="scripts/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-    <link rel="stylesheet" type="text/css" href="scripts/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $("a.photo").fancybox();
-        });
-    </script>
-    <title>Geek-php</title>
-</head>
-
-<body>
-    <div class="container">
-        <?php include "blocks/header.php"; ?>
-        <div class="row">
-            <div class="col-md-1">Left menu</div>
-            <?php include "blocks/content.php"; ?>
-            <div class="col-md-1">Right menu</div>
-        </div>
-        <div class="row">Footer</div>
-    </div>
-</body>
-
-</html>
