a039c133e6111597378d781784bb290a8426c0b3
pmaks <pmaks1985@gmail.com>
2021-03-13 12:37:43 +0300

    Главная, Войти, Регистрация переделал

--- v/v_login.php
+++ v/v_login.php
@@ -1,6 +1,18 @@
-<form method="post" id="form">
-	<input type="text" name="login" placeholder="Введите логин" required>
-	<input type="password" name="password" placeholder="Введите пароль" required>
-	<input type="submit" name="button" value="Войти">
-</form>
+<div class="row justify-content-center">
+    <div class="col-md-4">
+        <form method="post" id="form">
+            <div class="form-group">
+                <label for="login"><span class="font-weight-bold">Ваше имя:</span></label>
+                <input type="text" class="form-control" name="login" placeholder="Иван Иванов">
+            </div>
+            <div class="form-group">
+                <label for="pass"><span class="font-weight-bold">Ваш пароль:</span></label>
+                <input type="password" class="form-control" name="password">
+            </div>
+            <div class="form-group text-right">
+                <input type="submit" value="Войти" class="btn btn-primary">
+            </div>
+        </form>
+    </div>
+</div>
 <?php if(isset($text)){echo "<script>alert('$text');document.getElementById('form').style.display = 'none';</script>";}?>
--- v/v_reg.php
+++ v/v_reg.php
@@ -1,7 +1,26 @@
-<form method="post" id="form">
-	<input type="text" name="name" placeholder="Введите имя" required>
-	<input type="text" name="login" placeholder="Введите логин" required>
-	<input type="password" name="password" placeholder="Введите пароль" required>
-	<input type="submit" name="button" value="Зарегистрировать">
-</form>
-<?php if(isset($text)){echo "<script>alert('$text');document.getElementById('form').style.display = 'none';</script>";}?>
+<div class="row mt-5">
+    <h1>Регистрация пользователя</h1>
+</div>
+<div class="row">
+    <form method="post" id="form">
+        <div class="form-group">
+            <label for="fio">Ваше имя:<sup> *</sup>:</label>
+            <input type="text" class="form-control" id="fio" name="name">
+        </div>
+        <div class="form-group">
+            <label for="user-login">Ваш логин:<sup> *</sup>:</label>
+            <input type="text" class="form-control" id="user-login" name="login" required>
+        </div>
+        <div class="form-group">
+            <label for="user-pass">Ваш пароль:<sup> *</sup>:</label>
+            <input type="password" class="form-control" id="user-pass" name="password" required>
+        </div>
+        <div class="form-group">
+            <small><sup> *</sup>Поля, обязательные для заполнения</small>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Зарегистрироваться" name="button">
+    </form>
+</div>
+<?php if (isset($text)) {
+    echo "<script>alert('$text');document.getElementById('form').style.display = 'none';</script>";
+} ?>
