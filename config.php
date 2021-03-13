a3db37aa64936fc2944dbdebad6f3e0127afd046
pmaks <pmaks1985@gmail.com>
2021-03-13 14:52:29 +0300

    закончил, корзина выводиться не до конца

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-const SERVER = "localhost";
-const DB = "shop";
-const LOGIN = "root";
-const PASS = "root";
-
-$connect = mysqli_connect(SERVER, LOGIN, PASS, DB) or die("Ошибка соединения с базой данных");
