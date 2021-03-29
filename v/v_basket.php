736291b5a99390cfab703021de4dfdad75fc65a4
pmaks <pmaks1985@gmail.com>
2021-03-29 10:16:50 +0300

    думаю что не передается параметр из вьюшки метедом POST

--- v/v_basket.php
+++ v/v_basket.php
@@ -1,19 +1,19 @@
 <table border="1px">
-	<tr>
-		<th>Наименование товара</th>
-		<th>Количество (шт.)</th>
-		<th>Цена за ед. товара ($)</th>
-		<th>Сумма покупок($)</th>
-		<th>Действие</th>
-	</tr>
-	<?php
-		$order = 0;
-		if (isset($products)) {
-			foreach ($products as $product) {
-				echo "<tr><td>" . $product["title"] . "</td><td>" . $product["count"] . "</td><td>" . $product["price"] . "</td><td>" . $product["count"] * $product["price"] . "</td><td><form action='index.php?c=page&act=basket' method='post'><input type='submit' value='Удалить товар' class='btn btn-default'></form></td></tr>";
-				$order += $product["count"] * $product["price"];
-			}
-		}
-	?>
+    <tr>
+        <th>Наименование товара</th>
+        <th>Количество (шт.)</th>
+        <th>Цена за ед. товара ($)</th>
+        <th>Сумма покупок($)</th>
+        <th>Действие</th>
+    </tr>
+    <?php
+    $order = 0;
+    if (isset($products)) {
+        foreach ($products as $product) {
+            echo "<tr><td>" . $product["title"] . "</td><td>" . $product["count"] . "</td><td>" . $product["price"] . "</td><td>" . $product["count"] * $product["price"] . "</td><td><form action='index.php?c=page&act=clear' method='post'><input type='submit' value='Удалить товар' class='btn btn-default' id=" . $product["order_id"] . " name=" . $product["order_id"] . "></form></td></tr>";
+            $order += $product["count"] * $product["price"];
+        }
+    }
+    ?>
 </table>
-<h2><?= "Итоговая сумма заказов: " . $order . " USD"?></h2>
+<h2><?= "Итоговая сумма заказов: " . $order . " USD" ?></h2>
